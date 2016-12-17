d1e5d113414ccac0754efbbcad71f18ca0e9ae23
hyyl <hla163@sfu.ca>
2016-12-16 17:45:15 -0800

    basic table+record creation

--- main.py
+++ main.py
@@ -1,38 +1,8 @@
 import sqlite3
 
-def yesorno():
-    val=raw_input()
-    val=val[0]
-    if val=='y' or val=='Y':
-        return 1
-    elif val=='n' or val=='N':
-        return 0
-    else:
-        return -1
+#needed: interface, table creation, table add row, table read row
 
-def noninject(prompt):
-    return ''.join(x for x in prompt if (x.isalnum or x==" " or x=='-' or x=="." or x==":") )
-
-def addnew(c):
-    tablename=noninject(raw_input("Please enter the name of the illness "))
-    try:
-        c.execute('CREATE TABLE {tn} (date text,time text, rating real, other text)'.format(tn=tablename));
-        print "Now tracking", tablename+"."
-        return
-    except sqlite3.OperationalError:
-        print "That already exists. Use a different name."
-        addnew(c)
-    
-def addentries(tablename,c):
-    e_date=raw_input('date of illness? ')
-    e_time=raw_input('time of illness? ')
-    rating=int(raw_input('Rating of illness? 1-10 '))
-    othernotes=noninject(raw_input('anything else to say? '))
-    newentry=[tablename,e_date,e_time,rating,othernotes]
-    print "You were sick at", e_date, e_time, "with an intensity of", rating, ". Also,", othernotes+"."
-    c.execute("INSERT INTO ? VALUES(?,?,?,?)",newentry)
-
-
+#interface to add entry, add table, view in table, dump table, quit
 def choosemode():
     val=raw_input('Create new tracker (C), Log(L), View(V), or Print(P)? Or quit(Q).')
     if len(val)>1:
@@ -50,6 +20,34 @@ def choosemode():
     else:
         choosemode()
 
+#reduce SQL injection vulnerability
+def noninject(prompt):
+    return ''.join(x for x in prompt if (x.isalnum or x==" " or x=='-' or x=="." or x==":") )
+
+#CREATE: add a new table, checks for unique name. add the ability to exit
+def addnewtable(c):
+    tablename=noninject(raw_input("Please enter the name of the illness "))
+    try:
+        c.execute('CREATE TABLE {tn} (date text,time text, rating real, other text)'.format(tn=tablename));
+        print "Now tracking", tablename+"."
+        return
+    except sqlite3.OperationalError:
+        print "That already exists. Use a different name."
+        addnewtable(c)
+
+#WRITE: add a new entry to a given table.
+def addentries(tablename,c):
+    e_date=raw_input('date of illness? ')
+    e_time=raw_input('time of illness? ')
+    rating=int(raw_input('Rating of illness? 1-10 '))
+    othernotes=noninject(raw_input('anything else to say? '))
+    newentry=[e_date,e_time,rating,othernotes]
+    print "You were sick at", e_date, e_time, "with an intensity of", rating, ". Also,", othernotes+"."
+    c.execute("INSERT INTO {tn} VALUES(?,?,?,?)".format(tn=tablename),newentry)
+
+
+
+
 dbFilename='illnessdb.sqlite'
 
 con= sqlite3.connect(dbFilename)
@@ -65,7 +63,7 @@ mode='noidea'
 while(mode!='quit'):
     mode=choosemode()
     if mode=='create':
-        addnew(c)
+        addnewtable(c)
     if mode=='query':
         pass
     if mode=='dump':
@@ -83,32 +81,5 @@ while(mode!='quit'):
 
 
 
-conn.commit()
-conn.close()
-PROMPT_INVALID=True
-mode=''
-
-while(PROMPT_INVALID):
-    PROMPT_INVALID=False
-    val=raw_input('Log(L), View(V), or Print(P)?')
-    if val=='l' or val == 'L':
-        mode='write'
-    elif val=='v' or val == 'V':
-        mode='query'
-    elif val=='p' or val=='P':
-        mode='dump'
-    else:
-        PROMPT_INVALID=true;
-
-
-conn= sqlite3.connect(dbFilename)
-c=conn.cursor()
-
-if mode=='write':
-    date=raw_input('date of illness? ')
-    time=raw_input('time of illness? ')
-    rating=input('Rating of illness? 1-10 ')
-    othernotes=raw_input('anything else to say? ')
-    print "You were sick at", date, time, "with an intensity of", rating, ". Also,", othernotes, "."
-    
-'''
+con.commit()
+con.close()
