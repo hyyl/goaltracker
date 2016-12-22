11cf8d9f3dfa33cb947206936d84207bef20094f
hyyl <hla163@sfu.ca>
2016-12-21 17:39:40 -0800

    added calculation of end date

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sqlite3
 from datetime import *
 from dateutil.parser import *
+from dateutil.relativedelta import *
+import re
+import calendar
 
 #needed: interface, table creation, table add row, table read row
 
@@ -37,29 +40,62 @@ def addnewtable(c):
         print "That already exists. Use a different name."
         addnewtable(c)
 
-def parsedate(date):
-    if 'today' in date:
-        return dt.date
 
 def getdate():
     NOW=datetime.now()
     DATE_FORMAT="%a %b %d %Y at %H:%M"
-    retval=parse(raw_input('date and time of illness? '), ignoretz=True, fuzzy=True, default=NOW)
-    return retval.strftime(DATE_FORMAT)
+    try:
+        retval=parse(raw_input('date and time of illness? '), ignoretz=True, fuzzy=True, default=NOW)
+    except ValueError:
+        print "datefailed"
+    try:
+        print retval.strftime(DATE_FORMAT)
+        return retval.strftime(DATE_FORMAT)
+    except ValueError:
+        print "Please clarify your input."
+        getdate()
+
+def getduration(e_date,inpstring):
+    regexstring='(days)+|(day)+|(d)+|(hours)+|(hour)+|(hr)+|(hrs)+|(h)+|(minutes)+|(minute)+|(mins)+|(min)+|(m)+'
+    durarr=filter(None,re.split(regexstring,inpstring,flags=re.IGNORECASE))
+    length=len(durarr)
+    e_date=datetime.strptime(e_date,"%a %b %d %Y at %H:%M")
+    if length%2!=0 or length>6 or length==0: #strings that don't match won't work
+        return ValueError 
+    else:
+        if length==6:
+            return e_date+relativedelta(days=+int(durarr[0]),hours=+int(durarr[2]),minutes=+int(durarr[4]))
+        elif length==4:
+            if 'm' not in inpstring:
+                return e_date+relativedelta(days=+int(durarr[0]),hours=+int(durarr[2]))
+            if 'h' not in inpstring:
+                return e_date+relativedelta(days=+int(durarr[0]),minutes=+int(durarr[2]))
+            if 'd' not in inpstring:
+                return e_date+relativedelta(hours=+int(durarr[0]),minutes=+int(durarr[2]))
+        elif length==2:
+            if 'd' in inpstring:
+                return e_date+relativedelta(days=+int(durarr[0]))
+            if 'h' in inpstring:
+                return e_date+relativedelta(hours=+int(durarr[0]))
+            if 'm' in inpstring:
+                return e_date+relativedelta(minutes=+int(durarr[0]))
 
 #WRITE: add a new entry to a given table.
 def addentries(tablename,c):
+    e_date=getdate()
+    DATE_FORMAT="%a %b %d %Y at %H:%M"
     while True:
         try:
-            e_date=getdate()
+            e_duration_input=raw_input('duration of illness')
+            e_duration=getduration(e_date,e_duration_input).strftime(DATE_FORMAT)
             break
         except ValueError:
-            print "Please clarify your input."
-    e_duration=raw_input('duration of illness')
+            print "Please clarify your input in days, hours, and/or mintues"
     rating=int(raw_input('Rating of illness? 1-10 '))
     othernotes=noninject(raw_input('anything else to say? '))
     newentry=[e_date,e_duration,rating,othernotes]
-    print "You were sick at", e_date, "with an intensity of", rating, ". Also,", othernotes+"."
+    print "You were sick at", e_date, "until", e_duration, "with an intensity of", rating, ". Also,", othernotes+"."
+
     c.execute("INSERT INTO {tn} VALUES(?,?,?,?)".format(tn=tablename),newentry)
 
 
